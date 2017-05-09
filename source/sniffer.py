ef7470059a91e0e8e01597653e85529a1a4ba4b3
Lautaro <laucha@laucha-pc>
2017-05-09 18:50:49 -0300

    Agrego para guardar paquetes y el estado de los simbolos en un archivo de logs.

--- source/sniffer.py
+++ source/sniffer.py
@@ -1,18 +1,17 @@
 #! /usr/bin/env python
-import os, math
-import time
+import os, math, time, csv
 from operator import truediv
 from scapy.all import *
 
 # constantes
 WHO_HAS = 1
 IS_AT = 2
-MAC_BROADCAST = 'ff:ff:ff:ff:ff:ff'
+MAC_BROADCAST = "ff:ff:ff:ff:ff:ff"
 SIMBOLO_BROADCAST = "Broadcast        "
 SIMBOLO_UNICAST = "Unicast          "
 
 class sniffer:
-    def __init__(self, ejercicio):
+    def __init__(self, ejercicio, log_id):
         #-- generales
         self.ejercicio = ejercicio
         self.paquetes = []
@@ -20,6 +19,13 @@ class sniffer:
         if ejercicio == "1":
             self.simbolos[SIMBOLO_BROADCAST] = 0
             self.simbolos[SIMBOLO_UNICAST] = 0
+        if log_id:
+            #--vamos a guardar logs
+            self.log_id = log_id
+            self.log_paquetes = csv.writer(open(self.log_id + "_paquetes.csv", "wb"))
+            self.log_paquetes.writerow(["tiempo", "origen", "destino", "tipo"])
+            self.log_resultados = csv.writer(open(self.log_id + "_resultados.csv", "wb"))
+            self.log_resultados.writerow(["tiempo", "simbolo", "probabilidad", "informacion", "cantidad de paquetes", "entropia"])
 
         actualizar_pantalla(self)
         self.iniciar_sniff()
@@ -38,6 +44,9 @@ class sniffer:
                 if pkt[ARP].op == WHO_HAS:
                     simbolo = pkt.src
             if simbolo != "":
+                #--guarda logs
+                if self.log_paquetes:
+                    self.log_paquetes.writerow([time.strftime("%H:%M:%S"), pkt.src, pkt.dst, "WHO_HAS"])
                 self.paquetes.append(pkt)
                 #-- si no estaba en el diccionario lo crea
                 if simbolo not in self.simbolos.keys():
@@ -74,8 +83,9 @@ def actualizar_pantalla(sniff_obj):
             if probabilidad > 0 and len(sniff_obj.simbolos) > 1:
                 informacion = -math.log(probabilidad, len(sniff_obj.simbolos))
         print simbolo + "               " + str(probabilidad) + "                     " + str(informacion)
-        # escritura en el archivo para el experimentacion
-        os.system("echo "+time.strftime("%H:%M:%S")+","+simbolo+","+str(probabilidad)+","+str(informacion)+","+str(len(sniff_obj.paquetes))+","+str(entropia)+" >> resultado.csv")
+        #--guarda logs
+        if sniff_obj.log_resultados:
+            sniff_obj.log_resultados.writerow([time.strftime("%H:%M:%S"), simbolo, probabilidad, informacion, len(sniff_obj.paquetes), entropia])
     print ""
     print "--------------------------------------------------------------------"
     print "------------------------ ULTIMOS 10 PAQUETES -----------------------"
@@ -90,6 +100,6 @@ if usuario != "root":
     print "Correlo con root chabon!"
     exit(1)
 ejercicio = raw_input("Ingrese el numero de ejercicio: ")
-os.system("echo '' > resultado.csv")
-os.system("echo tiempo,simbolo,probabilidad,informacion,cantidad de paquetes,entropia >> resultado.csv")
-sniff_obj = sniffer(ejercicio)
+log_id = raw_input("Ingrese el identificador de log (ENTER si no guarda logs): ")
+
+sniff_obj = sniffer(ejercicio, log_id)
