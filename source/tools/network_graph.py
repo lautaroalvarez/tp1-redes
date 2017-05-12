ad6e8846407998931c52363275e01a82726f1446
Lautaro <laucha@laucha-pc>
2017-05-11 23:02:35 -0300

    Agrego cambios en graficador. Diferencia tamano de nodos y grosor de aristas segun el peso.

--- source/tools/network_graph.py
+++ source/tools/network_graph.py
@@ -11,19 +11,32 @@ import csv, os
 #   - IP
 #   - PESO: cantidad de paquetes que se enviaron entre ambos
 
+#-- un nodo suma peso si es el origen de un paquete
 SUMA_ORIGEN = 1
+#-- un nodo suma peso si es el destino de un paquete
 SUMA_DESTINO = 2
 
+#-- indice de columna del identificador de origen (ip o mac)
 INDEX_ORIGEN = 1
+#-- indice de columna del identificador de destino (ip o mac)
 INDEX_DESTINO = 2
+
+NODO_MAX_HEIGHT = 2
+NODO_MIN_FONTSIZE = 6
+NODO_MAX_FONTSIZE = 20
+
+ARISTA_MIN_WIDTH = 0.2
+ARISTA_MAX_WIDTH = 3
+ARISTA_COLOR = 'black'
 
 class graficador:
     def __init__(self, input_file):
         self.nodos = {}
         self.aristas = {}
         self.input_file = input_file
-        self.suma_peso = SUMA_DESTINO
+        self.suma_peso = SUMA_ORIGEN
 
+        self.maximo_peso = 0
         self.importarDatos()
 
     def importarDatos(self):
@@ -45,6 +58,9 @@ class graficador:
                 if self.suma_peso == SUMA_ORIGEN:
                     #-- se suma el paquete al nodo
                     self.nodos[paquete[INDEX_ORIGEN]]['peso'] += 1
+                    #-- se guarda el maximo peso
+                    if self.nodos[paquete[INDEX_ORIGEN]]['peso'] > self.maximo_peso:
+                        self.maximo_peso = self.nodos[paquete[INDEX_ORIGEN]]['peso']
                 #-- se guarda como minimo id de los dos nodos
                 nodo_min = self.nodos[paquete[INDEX_ORIGEN]]
 
@@ -59,6 +75,9 @@ class graficador:
                 if self.suma_peso == SUMA_DESTINO:
                     #-- se suma el paquete al nodo
                     self.nodos[paquete[INDEX_DESTINO]]['peso'] += 1
+                    #-- se guarda el maximo peso
+                    if self.nodos[paquete[INDEX_DESTINO]]['peso'] > self.maximo_peso:
+                        self.maximo_peso = self.nodos[paquete[INDEX_DESTINO]]['peso']
                 #-- se verifica si es el de minimo id de los dos nodos
                 if nodo_min['id'] > self.nodos[paquete[INDEX_DESTINO]]['id']:
                     nodo_max = nodo_min
@@ -84,13 +103,20 @@ class graficador:
         f = open('graph.tmp','w')
         f.writelines('digraph G {\n')
         for i in self.nodos:
-            f.writelines('"' + self.nodos[i]['ip'] + '" [label="' + self.nodos[i]['ip'] + '"]\n')
+            #-- calcula tamanos de nodo y fuentes
+            peso_nodo = self.nodos[i]['peso']
+            height = peso_nodo * NODO_MAX_HEIGHT / self.maximo_peso
+            font_size = (peso_nodo * (NODO_MAX_FONTSIZE - NODO_MIN_FONTSIZE) / self.maximo_peso) + NODO_MIN_FONTSIZE
+            f.writelines('"' + self.nodos[i]['ip'] + '" [label="' + self.nodos[i]['ip'] + '",fontsize=' + str(font_size) + ',height=' + str(height) + ',regular=true]\n')
         for i in self.aristas:
             for j in self.aristas[i]['aristas']:
-                f.writelines('"' + self.aristas[i]['ip'] + '" -> "' + self.aristas[i]['aristas'][j]['ip'] + '" [dir=none,penwidth=0.5,color=black];\n')
+                #-- calcula ancho de aristas1
+                peso_arista = self.aristas[i]['aristas'][j]['peso']
+                width = (peso_arista * (ARISTA_MAX_WIDTH - ARISTA_MIN_WIDTH) / self.maximo_peso) + ARISTA_MIN_WIDTH
+                f.writelines('"' + self.aristas[i]['ip'] + '" -> "' + self.aristas[i]['aristas'][j]['ip'] + '" [dir=none,penwidth=' + str(width) + ',color=' + ARISTA_COLOR + '];\n')
         f.writelines('}')
         f.close()
-        os.system("dot -Tjpg -o" + output_file + ".jpg graph.tmp")
+        os.system("dot -Tpng -o" + output_file + ".png graph.tmp")
         os.system("rm graph.tmp")
 
 
