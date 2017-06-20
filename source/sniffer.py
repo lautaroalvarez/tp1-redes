a56ef04c12844eb687b2a17db58cefcd9a8b4b8e
matias <matias@la-maquina-de-dios>
2017-06-20 17:41:21 -0300

    saque los modos, si estas al pedo refactoriza el codigo

--- source/sniffer.py
+++ source/sniffer.py
@@ -2,6 +2,7 @@
 import os, math, time, csv
 from operator import truediv
 from scapy.all import *
+import collections
 
 # constantes
 WHO_HAS = 1
@@ -9,17 +10,16 @@ IS_AT = 2
 MAC_BROADCAST = "ff:ff:ff:ff:ff:ff"
 SIMBOLO_BROADCAST = "Broadcast        "
 SIMBOLO_UNICAST = "Unicast          "
-NOMBRES_MODOS = ["","Broadcast vs Unicast", "Diferenciar Hosts"]
 
 class sniffer:
-    def __init__(self, modo, log_id):
+    def __init__(self,log_id):
         #-- generales
-        self.modo = int(modo)
         self.paquetes = []
+        self.paquetesWH = []
         self.simbolos = {}
-        if modo == 1:
-            self.simbolos[SIMBOLO_BROADCAST] = 0
-            self.simbolos[SIMBOLO_UNICAST] = 0
+        self.simbolos = collections.OrderedDict(self.simbolos)
+        self.simbolos[SIMBOLO_BROADCAST] = 0
+        self.simbolos[SIMBOLO_UNICAST] = 0
         self.log_id = 0
         if log_id:
             #--vamos a guardar logs
@@ -36,21 +36,28 @@ class sniffer:
     def recibir_paquete(self, pkt):
         if ARP in pkt: # es arp
             simbolo = ""
-            if modo == 1:
-                #-- ejercicio 1
-                if pkt.dst == MAC_BROADCAST:
-                    simbolo = SIMBOLO_BROADCAST
-                else:
-                    simbolo = SIMBOLO_UNICAST
+            #-- ejercicio 1
+            if pkt.dst == MAC_BROADCAST:
+                simbolo = SIMBOLO_BROADCAST
             else:
-                #-- ejercicio 2
-                if pkt[ARP].op == WHO_HAS:
-                    simbolo = pkt[ARP].psrc
+                simbolo = SIMBOLO_UNICAST
             if simbolo != "":
                 #--guarda logs
                 if self.log_id:
-                    self.log_paquetes.writerow([time.strftime("%H:%M:%S"), pkt[ARP].psrc, pkt.src, pkt[ARP].pdst, pkt.dst, self.modo])
+                    self.log_paquetes.writerow([time.strftime("%H:%M:%S"), pkt[ARP].psrc, pkt.src, pkt[ARP].pdst, pkt.dst,1])
                 self.paquetes.append(pkt)
+                #-- si no estaba en el diccionario lo crea
+                if simbolo not in self.simbolos.keys():
+                    self.simbolos[simbolo] = 0
+                #-- se suma el paquete
+                self.simbolos[simbolo] += 1
+            #-- ejercicio 2
+            if pkt[ARP].op == WHO_HAS:
+                simbolo = pkt[ARP].psrc
+                #--guarda logs
+                if self.log_id:
+                    self.log_paquetes.writerow([time.strftime("%H:%M:%S"), pkt[ARP].psrc, pkt.src, pkt[ARP].pdst, pkt.dst,2])
+                self.paquetesWH.append(pkt)
                 #-- si no estaba en el diccionario lo crea
                 if simbolo not in self.simbolos.keys():
                     self.simbolos[simbolo] = 0
@@ -68,31 +75,47 @@ def actualizar_pantalla(sniff_obj):
         sniff_obj.ultimo_log = time.time()
         loguear = 1
     print "--------------------------------------------------------------------"
-    print "-- Modo: " + NOMBRES_MODOS[sniff_obj.modo]
     print "-- Cantidad de Simbolos: " + str(len(sniff_obj.simbolos))
     print "-- Cantidad de Paquetes: " + str(len(sniff_obj.paquetes))
     entropia = "-"
+    entropiaUniBro = "-"
     if len(sniff_obj.paquetes) > 0 and len(sniff_obj.simbolos) > 1:
         entropia = 0
-        for simbolo in sniff_obj.simbolos:
-            if sniff_obj.simbolos[simbolo] > 0:
-                probabilidad = truediv(sniff_obj.simbolos[simbolo], len(sniff_obj.paquetes))
-                entropia += (-math.log(probabilidad, len(sniff_obj.simbolos)) * probabilidad)
-    print "-- Entropia: " + str(entropia)
+        entropiaUniBro = 0
+        print sniff_obj.simbolos
+        for i in range(0, len(sniff_obj.simbolos)):
+            simbolo = sniff_obj.simbolos.keys()[i]
+            if i < 2:
+                if sniff_obj.simbolos[simbolo] > 0:
+                    probabilidadUniBro = truediv(sniff_obj.simbolos[simbolo],len(sniff_obj.paquetes))
+                    entropiaUniBro += (-math.log(probabilidadUniBro, 2) * probabilidadUniBro)
+            else:
+                if len(sniff_obj.simbolos) > 3:
+                    if sniff_obj.simbolos[simbolo] > 0:
+                        probabilidad = truediv(sniff_obj.simbolos[simbolo], len(sniff_obj.paquetesWH))
+                        entropia += (-math.log(probabilidad, len(sniff_obj.simbolos)-2) * probabilidad)
+    print "-- Entropia IPs: "+str(entropia)
+    print "-- Entropia unicast vs broadcast: " + str(entropiaUniBro)
     print "--------------------------------------------------------------------"
     print "--------------------------------------------------------------------"
     print "Simbolo                    Probabilidad                  Informacion"
-    for simbolo in sniff_obj.simbolos:
+    for i in range(0,len(sniff_obj.simbolos)):
+        simbolo = sniff_obj.simbolos.keys()[i]
         probabilidad = "-"
         informacion = "-"
         if len(sniff_obj.paquetes) > 0:
-            probabilidad = truediv(sniff_obj.simbolos[simbolo], len(sniff_obj.paquetes))
-            if probabilidad > 0 and len(sniff_obj.simbolos) > 1:
-                informacion = -math.log(probabilidad, len(sniff_obj.simbolos))
+            if i < 2:
+                probabilidad = truediv(sniff_obj.simbolos[simbolo], len(sniff_obj.paquetes))
+                if probabilidad > 0 and len(sniff_obj.simbolos) > 1:
+                    informacion = -math.log(probabilidad, len(sniff_obj.simbolos))
+            else:
+                probabilidad = truediv(sniff_obj.simbolos[simbolo], len(sniff_obj.paquetesWH))
+                if probabilidad > 0 and len(sniff_obj.simbolos)-2 > 1:
+                    informacion = -math.log(probabilidad, len(sniff_obj.simbolos)-2)
         print simbolo + "               " + str(probabilidad) + "                     " + str(informacion)
         #--guarda logs
         if loguear:
-            sniff_obj.log_resultados.writerow([time.strftime("%H:%M:%S"), simbolo, probabilidad, informacion, len(sniff_obj.paquetes), entropia])
+            sniff_obj.log_resultados.writerow([time.strftime("%H:%M:%S"), simbolo.strip(), probabilidad, informacion, len(sniff_obj.paquetes), entropia])
     print ""
     print "--------------------------------------------------------------------"
     print "------------------------ ULTIMOS 10 PAQUETES -----------------------"
@@ -106,10 +129,6 @@ usuario = usuario[:-1] # saco el eol del final
 if usuario != "root":
     print "Correlo con root chabon!"
     exit(1)
-print "Elija un modo:"
-print "1. Broadcast vs Unicast"
-print "2. Diferenciar Hosts"
-modo = raw_input("Opcion elegida: ")
 log_id = raw_input("Ingrese el identificador de log (ENTER si no guarda logs): ")
 
-sniff_obj = sniffer(modo, log_id)
+sniff_obj = sniffer(log_id)
